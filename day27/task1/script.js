a7a9a0a2e74c70d995eee8f227e6f1879bb7a795
Nandana-Rayaroth <nandana.rayaroth@vonnue.com>
2025-11-24 21:29:27 +0530

    day27secondcommit

--- day27/task1/script.js
+++ day27/task1/script.js
@@ -4,3 +4,15 @@ const Close1 = document.getElementById("close1");
 Close1.addEventListener("click", function(){
     notification1.style.display = "none"
 })
+
+const notification2 = document.getElementById("notification2");
+const close2 = document.getElementById("close2");
+close2.addEventListener("click", function(){
+    notification2.style.display = "none"
+})
+
+const notification3 = document.getElementById("notification3");
+const close3 = document.getElementById("close3");
+close3.addEventListener("click", function(){
+    notification3.style.display = "none"
+})
